3e7efca45fcf11595898ea097afeb156d4d33ed0
danushkacodeolima <danushka@codeolima.com>
2024-10-07 10:33:43 +0530

    Initial Commit

---
+++ Models/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentApplication.Models.Entities
+{
+    public class Student
+    {
+        public Guid StudentID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string StudentEmail { get; set; }
+        public required string Phone { get; set; }
+        public string? Address { get; set; }
+        public required string Country { get; set; }
+        public required string Institute { get; set; }
+        public required string Intake { get; set; }
+        public required string CourseTitle { get; set; }
+
+    }
+}
