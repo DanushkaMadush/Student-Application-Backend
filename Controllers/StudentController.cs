7191d9be000e593958cbcea9876366f0c6bb1649
danushkacodeolima <danushka@codeolima.com>
2024-10-07 11:54:06 +0530

    addstudent API created

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StudentApplication.Data;
+using StudentApplication.Models;
+using StudentApplication.Models.Entities;
 
 namespace StudentApplication.Controllers
 {
@@ -20,6 +22,28 @@ namespace StudentApplication.Controllers
         {
             var allStudents = dbContext.Students.ToList();
             return Ok(allStudents);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(AddStudentDto addStudentDto)
+        {
+            var studentEntity = new Student()
+            {
+                FirstName = addStudentDto.FirstName,
+                LastName = addStudentDto.LastName,
+                StudentEmail = addStudentDto.StudentEmail,
+                Phone = addStudentDto.Phone,
+                Address = addStudentDto.Address,
+                Country = addStudentDto.Country,
+                Institute = addStudentDto.Institute,
+                Intake = addStudentDto.Intake,
+                CourseTitle = addStudentDto.CourseTitle,
+            };
+
+            dbContext.Students.Add(studentEntity);
+            dbContext.SaveChanges();
+
+            return Ok(studentEntity);
         }
     }
 }
